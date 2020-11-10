9a5c509d0fada9a8d312c2d081c96455d870bf28
Brian Demro <demrobrian@hotmail.com>
2020-11-10 13:30:52 -0600

    some more stuff

--- server/index.js
+++ server/index.js
@@ -30,27 +30,29 @@ massive({
 app.post('/auth/signup', async (req, res) => {
   let { email, password } = req.body;
   let db = req.app.get('db')
-  let userFound = await db.check_user_exists([email]);
-  if (userFound[0]) {
+  let [userFound] = await db.check_user_exists([email]);
+  if (userFound) {
     return res.status(200).send('Email already exists')
   }
   let salt = bcrypt.genSaltSync(10);
   let hash = bcrypt.hashSync(password, salt);
-  let createdUser = await db.create_customer([email, hash])
-  req.session.user = { id: createdUser[0].id, email: createdUser[0].email }
+  let [createdUser] = await db.create_customer([email, hash])
+  req.session.user = { id: createdUser.id, email: createdUser.email }
   res.status(200).send(req.session.user)
 });
 
 app.post('/auth/login', async (req, res) => {
-  let { email, password } = req.body;
   let db = req.app.get('db')
-  let userFound = await db.check_user_exists(email)
-  if (!userFound[0]) {
+  let { email, password } = req.body;
+  let [userFound] = await db.check_user_exists(email)
+  if (!userFound) {
     return res.status(200).send('Incorrect email. Please try again.');
   }
-  let result = bcrypt.compareSync(password, userFound[0].user_password)
-  if (result) {
-    req.session.user = { id: userFound[0].id, email: userFound[0].email }
+  let authenticated = bcrypt.compareSync(password, userFound.user_password)
+  if (authenticated) {
+    req.session.user = { 
+      id: userFound.id, 
+      email: userFound.email }
     res.status(200).send(req.session.user)
   } else {
     return res.status(401).send('Incorrect email/password')
@@ -62,13 +64,7 @@ app.get('/auth/logout', (req, res) => {
   res.sendStatus(200);
 });
 
-app.get('/auth/user', (req, res) => {
-  if (req.session.user) {
-    res.status(200).send(req.session.user)
-  } else {
-    res.status(401).send('please log in')
-  }
-});
+
 
 app.listen(SERVER_PORT, () => {
   console.log(`Listening on port: ${SERVER_PORT}`);
